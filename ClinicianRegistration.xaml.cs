61d08b1b810118900b7bffb1b698b1ca826b6795
Joshua DAgostino <123174712+jdagostino02@users.noreply.github.com>
2024-02-28 15:31:59 -0500

    Upload rest of login portal

---
+++ ClinicianRegistration.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YourAppName
+{
+    /// <summary>
+    /// Interaction logic for ClinicianRegistration.xaml
+    /// </summary>
+    public partial class ClinicianRegistrationWindow : Window
+    {
+        public ClinicianRegistrationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    private void RegisterButton_Click(object sender, RoutedEventArgs e)
+    {
+        string email = txtEmail.Text;
+        string password = txtPassword.Password;
+        string firstName = txtFirstName.Text;
+        string lastName = txtLastName.Text;
+        DateTime birthDate = dpBirthDate.SelectedDate.Value;
+        int npiNumber = int.Parse(txtNpiNumber.Text);
+        string employerName = txtEmployerName.Text;
+
+        //Save clinician to database
+
+        Close();
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net.Configuration;
+using System;
+
+namespace YourAppName
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string connectionString = "Server=localhost; Database=master; Trusted_Connection=True";
+        // In a real-world scenario, replace this with actual authentication logic
+        private bool AuthenticateUser(string username, string password)
+        {
+            string query = "SELECT COUNT(*) FROM Users WHERE Username = @username AND Password = @password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+                                {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", (password));
+
+                    connection.Open();
+
+                    int userCount = (int)command.ExecuteScalar();
+
+                    return userCount == 1;
+                }
+            }   
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            if (AuthenticateUser(username, password))
+            {
+                // Successful login
+                txtMessage.Text = "Login successful!";
+                // Add logic to navigate to the next part of your application
+            }
+            else
+            {
+                // Failed login
+                txtMessage.Text = "Invalid username or password. Please try again.";
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationPortal registrationPortal = new RegistrationPortal();
+            registrationPortal.Show();
+        }
+    }
+}
