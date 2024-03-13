e88902580e9b53d7d2c90fb390babb63e55d51b1
jdago <115362953+jdago@users.noreply.github.com>
2024-03-13 14:21:08 -0400

    Update DashboardWindow.xaml.cs

--- DashboardWindow.xaml.cs
+++ DashboardWindow.xaml.cs
@@ -1,18 +1,36 @@
 // DashboardWindow.xaml.cs
 
+//Most of the methods will have to be scrapped due to noncompliance to FHIR standard, unless there are future revisions being done
+//Currently looking at libraries and source code that incorporates R4-standard for patient-clinician messaging
+//Existing built-in libraries such as Cryptography, Security, HTTP can assist in securing communication over HTTPS
+
 using System;
 using System.Windows;
 using System.Data.SqlClient;
 using System.Collections.ObjectModel;
 
-namespace YourAppName
+//common namespace for the application
+namespace FHIRMessagingApplication
 {
     public partial class DashboardWindow : Window
     {
-        // Connection string to database
-        private readonly string connectionString = "Server=localhost; Database=YourDatabase; Trusted_Connection=True";
+        //Warning: There is an arbitrary value {Database=YourDatabase} should have a connection key (for local testing) or the endpoint of the instance
+        //Since it is an endpoint, to connect to the EC2 instance (helpful for viewing files, information in the container)
+        //1) Install Remote Development extension pack
+        //2) Open command palette and select Remote SSH: Add New SSH Host
+        //3) In the "Add New SSH Host Ddialog", this is where we will put the IP address associated with the instance
+        //4) Click Add
+        //5) Open command palette and select the "Remote SSH: Connect to Host..." command
+        //6) Select the host which will be the EC2 instance
+        //Note: An SSH key pair must be provided to connect! This will be provided closer to the testing of the application
+
+        //This must be replaced by a connection to the EC2 instance (different file) that utilizes TLS 
+       // private readonly string connectionString = "Server=localhost; Database=YourDatabase; Trusted_Connection=True";
 
         // Collections to hold data for different sections
+        //Not sure about collections holding the necessary information, as it must be serialized in JSON (for HTTP POST request)
+        //Then deserialized
+        //Also, not sure how the collection will conform to the data model for class Patient
         private ObservableCollection<PatientRecord> patientRecords;
         private ObservableCollection<Message> messages;
         private ObservableCollection<Patient> patients;
@@ -25,6 +43,7 @@ namespace YourAppName
         }
 
         // Method to initialize data for each section
+        //Looks like this assume that the 
         private void InitializeData()
         {
             LoadPatientRecords();
@@ -34,6 +53,9 @@ namespace YourAppName
         }
 
         // Method to load patient records
+        //Entire method needs to be redone, as it is not FHIR compliant.
+        //Like mentioned earlier, 1) exchanges must be done using TLS handshakes; 2) data format must be in FHIR-approved form; and 3) encryption is vital to protecting information
+        
         private void LoadPatientRecords()
         {
             patientRecords = new ObservableCollection<PatientRecord>();
@@ -59,6 +81,7 @@ namespace YourAppName
                             Id = reader.GetInt32(0),
                             Name = reader.GetString(1),
                             // Add other properties as needed
+                            
                         });
                     }
                 }
@@ -70,6 +93,7 @@ namespace YourAppName
         }
 
         // Method to load messages
+    
         private void LoadMessages()
         {
             messages = new ObservableCollection<Message>();
